599849fb95a44312047e495fea412a786bbdd949
XiongYuSong <937724308@qq.com>
2018-07-17 11:18:48 +0800

    第一次代码提交

---
+++ test_1/spider/webbserver.js
@@ -0,0 +1,50 @@
+"user strict"
+var port,server,service,system = require('system');
+var fs = require('fs');
+var root = fs.absolute("./data");
+fs.changeWorkingDirectory(root);
+console.log('workingDirectory'+root);
+
+
+var page = require('webpage').create();
+if(system.args.length !==2){
+    console.log('Usage:serverkeeplive.js<portnumber>');
+    phantom.exit(1);
+} else {
+    port = system.args[1];
+    server = require('webserver').create();
+
+    service = server.listen(port,function (request,response) {
+        var requestUrl = request.post.url;
+        response.headers = {
+            'Cache':'no-cache',
+            'Content-type':'text/html;charser=utf-8'
+        };
+        if (requestUrl){
+            page.open(requestUrl,function (status) {
+                if (status!=='success'){
+                    console.log('FAIL to load the address');
+                    return;
+                }
+                var body = page.content;
+                //写入文件
+                fs.write('demo.html',body,'w');
+                response.write(body);
+                response.close();
+            });
+        } else {
+            var body = 'no spider url';
+            response.write(body);
+            response.close();
+        }
+
+    });
+
+    if (service){
+        console.log('Web server running on port'+port);
+    } else {
+        console.log('Error: Could not create web server listening on port'+port);
+        phantom.exit();
+    }
+}
+
